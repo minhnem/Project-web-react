229a04ae243571a7a4fdc40aca8e336f5084c817
minhnem <dtc2154801030092@ictu.edu.vn>
2024-10-01 22:58:59 +0700

    update04

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
-
 // Xuất hàm server để Vercel có thể sử dụng
 export default (req, res) => {
-  server(req, res); // Gọi server để xử lý yêu cầu
   router(req, res);
 };
