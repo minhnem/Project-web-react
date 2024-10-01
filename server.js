e080de2e42a8f57e0ded5aaf110776ce4da68423
minhnem <dtc2154801030092@ictu.edu.vn>
2024-10-01 22:40:30 +0700

    update02

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server'; // Nếu dùng ES module
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+
 server.listen(3000, () => {
   console.log('JSON Server is running');
 });
 
-export default server
+// Xuất hàm server để Vercel có thể sử dụng
+export default (req, res) => {
+  // Thay vì gọi server.listen(), hãy sử dụng router
+  router(req, res);
+};
